ae526f133ea6e6f556187aa23317a021926b449c
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-06-11 21:18:51 -0700

    basic file structure

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const frontEndRoutes = require("./frontendRoutes.js")
+
+router.use('/api', apiRoutes);
+router.use( frontEndRoutes);
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'secret stuff',
+  cookie: {
+    maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"))
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
